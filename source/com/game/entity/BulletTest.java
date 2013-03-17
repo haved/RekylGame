9a315bd36213e4d78a3d2ff937d095d43794b0ae
haved <krogstie.havard@gmail.com>
2013-03-17 21:36:16 +0100

    Bullets die on impact

--- source/com/game/entity/BulletTest.java
+++ source/com/game/entity/BulletTest.java
@@ -1,9 +1,10 @@
 package com.game.entity;
 
 import com.game.RenderEngine;
+import com.game.world.World;
 
 public class BulletTest extends EntityBullet
-{	
+{
 	public BulletTest(int x, int y, int xSpeed, int ySpeed)
 	{
 		this.x = x - 2;
@@ -12,8 +13,26 @@ public class BulletTest extends EntityBullet
 		this.xSpeed = xSpeed;
 		this.ySpeed = ySpeed;
 		
+		if(xSpeed == 0){xSpeed++;}
+		if(ySpeed == 0){ySpeed++;}
+		
 		xSize = 4;
 		ySize = 4;
+		
+		hasCollision = true;
+	}
+	
+	public void tick(World w)
+	{
+		super.tick(w);
+		if(xSpeed == 0)
+		{
+			kill();
+		}
+		if(ySpeed == 0)
+		{
+			kill();
+		}
 	}
 	
 	public void render()
