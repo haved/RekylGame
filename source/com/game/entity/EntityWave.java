5592022012910ef3dbe099d5e16b1cb36ba623ea
haved <krogstie.havard@gmail.com>
2013-03-17 16:00:23 +0100

    The wave speeds up before when it eats you

--- source/com/game/entity/EntityWave.java
+++ source/com/game/entity/EntityWave.java
@@ -8,15 +8,24 @@ public class EntityWave extends Entity
 	private static int maxDistance = 300;
 	private static int normalSpeed = 450;
 	
+	private boolean hasWon;
+	
 	public EntityWave()
 	{
-		xSize = 280;
+		xSize = 480;
 		ySize = 512;
 		x = -maxDistance - xSize;
 	}
 	
 	public void tick(World world)
 	{
+		if(hasWon)
+		{
+			xSpeed += 50;
+			move(world);
+			return;
+		}
+		
 		if(xSpeed > normalSpeed)
 		{
 			xSpeed = Math.max(normalSpeed, xSpeed - 20);
@@ -40,15 +49,21 @@ public class EntityWave extends Entity
 	public void customRender()
 	{
 		RenderEngine.bindTexture("sprites.png");
-		RenderEngine.drawTransparentTexture(x + 200, y, 180, 512,
+		RenderEngine.drawTransparentTexture(x + 400, y, 180, 512,
 				208, 0, 48, 96);
 		RenderEngine.setGLColor(0, 0, 0, 1);
-		RenderEngine.fillRect(x-1000, 0, 1200, 512);
+		RenderEngine.fillRect(x-1000, 0, 1400, 512);
 	}
 	
 	@Override
 	public void onCollisionWithPlayer(World world, EntityPlayer player)
 	{
+		if(player.x - x < 50)
+		{
+			world.loose();
+		}
+		
+		hasWon = true;
 		player.stop();
 	}
 }
