37f796f4d64cee295022a6705fd49eef356aec10
haved <krogstie.havard@gmail.com>
2013-03-22 17:27:50 +0100

    Changed name of TextField class

---
+++ source/com/game/guiObject/GuiObjTextField.java
@@ -0,0 +1,34 @@
+package com.game.guiObject;
+
+public class GuiObjTextField extends GuiObject
+{
+	private String text;
+	
+	public GuiObjTextField()
+	{
+		
+	}
+	
+	public String getText()
+	{
+		return text;
+	}
+	
+	public void setText(String text)
+	{
+		this.text = text;
+	}
+	
+	@Override
+	public void tick()
+	{
+		
+	}
+	
+	@Override
+	public void render()
+	{
+		
+	}
+
+}
